9340b128abb5ae2ffda611c656d4b2b5e5ba8635
Sang Choi <akasyashc@gmail.com>
2016-12-20 14:03:22 -0800

    Redux-form template improvement

--- src/components/forms/form.js
+++ src/components/forms/form.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+const renderInput = field =>
+    <div>
+        <input {...field.input} type={field.type} />
+        {field.meta.touched &&
+         field.meta.error &&
+         <span className='error'>{field.meta.error}</span>}
+    </div>
+
 class Form extends Component {
     render() {
-
+        return(
+            <form className='form-name'>
+            </form>
+        );
     }
 }
 
